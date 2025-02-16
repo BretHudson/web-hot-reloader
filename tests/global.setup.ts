957547f9cacd6fb5b63413e98bf80c66bca0f822
Bret Hudson <bret@brethudson.com>
2025-02-16 02:22:39 -0800

    Set up Playwright

---
+++ tests/global.setup.ts
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+
+import { expect, test as setup } from './fixtures';
+import { SERVER_PORT, tempRoot, WHR_PORT } from './shared';
+
+setup('setup', async ({ request }) => {
+	if (!fs.existsSync(tempRoot)) {
+		fs.mkdirSync(tempRoot);
+	}
+
+	// ensure both servers are up & running
+	{
+		const url = `http://localhost:${SERVER_PORT}`;
+		const res = await request.head(url);
+		expect(res.ok()).toBeTruthy();
+	}
+	{
+		const url = `http://localhost:${WHR_PORT}/reloader.js`;
+		const res = await request.head(url);
+		expect(res.ok()).toBeTruthy();
+	}
+});
---
+++ tests/shared.ts
@@ -0,0 +1,14 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const rootPath = path.join(__dirname, '..');
+
+export const SERVER_PORT = 3030;
+export const WHR_PORT = 3040;
+
+export const tempDir = 'temp3';
+export const siteRoot = path.join(rootPath, 'tests/site');
+export const templateRoot = path.join(siteRoot, 'template');
+export const tempRoot = path.join(siteRoot, tempDir);
